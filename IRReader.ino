d462c6f7be81c72b7928a4e93cedd04758737b67
bernardd <bgithub@guarana.org>
2014-04-26 22:38:59 +1000

    Complete control set decoded
    I think I've actually got everything mapped now, except for programming
    which uses a much larger signal packet and will mean tweaking the way
    the reader works a bit.

--- IRReader.ino
+++ IRReader.ino
@@ -1,8 +1,5 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
@@ -31,32 +28,21 @@ void loop() {
     data[i] = 0;
   for (int i=0; i<3; i++)
     gaps[i] = 0;
-/*  while (digitalRead(IR)) {}
-  int pos = 0;
-  
-  digitalWrite(led, HIGH);
-  unsigned long t = 1;
-  t = pulseIn(IR, LOW);
-  data[pos++] = (unsigned int)t;
-  while (t) {
-    t = pulseIn(IR, HIGH);
-    data[pos++] = (unsigned int)t;
-    t = pulseIn(IR, LOW, (unsigned long)(2000*1000));
-    data[pos++] = (unsigned int)t;
-  }
-  digitalWrite(led, LOW);
-  */
+
   unsigned long t=1;
   unsigned int pos = 0;
   unsigned int gap = 0;
   while (digitalRead(IR)) {}
   
   digitalWrite(led, HIGH);
-  while (t) {
+  while (t && pos < L) {
     t = pulseIn(IR, HIGH);
     data[pos++] = (unsigned int)t;
   }
   digitalWrite(led, LOW);
+  
+  if (pos == L)
+    Serial.println("Warning: Storage exhausted!");
   
   int bits = 0;
   for (int i=0; i<pos; i++) {
@@ -68,7 +54,7 @@ void loop() {
       Serial.print(1);
       bits++;
     } else {
-      Serial.print('-');
+      Serial.println();
       gaps[gap++] = d;
     }
     if (d > 250 && d < 900) {
@@ -76,39 +62,7 @@ void loop() {
       Serial.println(d);
     }
   }
-  
-  /*
-  for (int i=0; i<pos; i++) {
-    if (!(i%2))
-      Serial.print('*');
-    Serial.print(data[i]);
-    Serial.print(',');
-  }
-  Serial.println();
-  
-  
-  int bits = 0;
-  int gap = 0;
-  for (int i=0; i<pos; i++) {
-    if (i%2) {
-      unsigned long d = data[i];
-      if (d < 500) {
-        Serial.print(0);
-        bits++;
-      } else if (d < 5000) {
-        Serial.print(1);
-        bits++;
-      } else {
-        Serial.print('-');
-        gaps[gap++] = d;
-      }
-      if (d > 250 && d < 900) {
-        Serial.print("Warning: ambiguous pulse length ");
-        Serial.println(d);
-      }
-    }
-  }
-  */
+ 
   Serial.print("\nGaps: ");
   for (int i=0; i<3; i++) {
     Serial.print(gaps[i]);
